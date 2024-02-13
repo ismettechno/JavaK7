00cf3f172c5222fc6de931fc4f35ca05d7219de2
TechnoStudy <ismettechno@gmail.com>
2024-02-13 16:34:12 +0000

    ders 1 b

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,7 +2,8 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
-        System.out.println("Merhaba 1");
+        for (int i = 0; i < 5; i++)
+            System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
 
     }
